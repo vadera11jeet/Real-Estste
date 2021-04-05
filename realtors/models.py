ee8c0797bdfb44aee20e74c3a5d43728d854505e
jeet <vadera11jeet@gmail.com>
2021-04-05 23:44:13 +0530

    admin area frontend improve and functionality added

--- realtors/models.py
+++ realtors/models.py
@@ -9,6 +9,6 @@ class Realtor(models.Model):
     email = models.CharField(max_length=50)
     is_mvp = models.BooleanField(default=False)
     hire_date = models.DateTimeField(default=datetime.now, blank=True)
-
+#  this method is used display value in admin section here we add name so this will show realtors name in admin section
     def __str__(self):
         return self.name
