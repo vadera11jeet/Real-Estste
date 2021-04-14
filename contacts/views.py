fb9703864ceadbd37fd6ba5abfb404a887967642
jeet <vadera11jeet@gmail.com>
2021-04-14 19:07:38 +0530

    user dashboard is working

--- contacts/views.py
+++ contacts/views.py
@@ -13,6 +13,13 @@ def contact(request):
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
 
+        if request.user.is_authenticated:
+            user_id = request.user.id
+            has_contaced = Contact.objects.all().filter(listing_id = listing_id, user_id =user_id)
+            if has_contaced:
+                messages.error(request, 'you have already made an requeset')
+                return redirect('/listings/'+ listing_id)
+
         contact = Contact(listing=listing_name, listing_id=listing_id, name=name, email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
         messages.success(request, "your request has been submitted a realtor will get back to you")
